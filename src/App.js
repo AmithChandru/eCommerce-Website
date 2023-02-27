8b393035f310bff3976717d2d82acb89c96f1927
AmithChandru <amithc279@gmail.com>
2023-02-28 02:25:20 +0530

    Completed website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const productsArr = [
+    {
+      title: 'Colors',
+      price: 100,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+    },
+    {
+      title: 'Black and white Colors',
+      price: 50,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+    },
+    {
+      title: 'Yellow and Black Colors',
+      price: 70,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+    },
+    {
+      title: 'Blue Color',
+      price: 100,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
+    }
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span>HOME</span>
+        <span style={{ margin: '0px 40px' }}>STORE</span>
+        <span>ABOUT</span>
+        <button className='cartButton'>Cart</button>
+        <span className='cartCount'>0</span>
       </header>
+      <div className='title'>
+        The Generics
+      </div>
+      <section className='products'>
+        <span style={{fontWeight: 'bold'}}>Colors</span>
+        <div className='content'>
+          {productsArr.map((item) => {
+            return (
+              <div>
+                <span style={{fontSize: '20px', fontWeight: 'bold'}}>{item.title}</span>
+                <div className='imageContainer'>
+                  <img className='image' src={`${item.imageUrl}`} alt='Color' />
+                </div>
+                <div style={{flexDirection: 'row', display: 'flex', justifyContent: 'space-between'}}>
+                  <span style={{fontSize: '20px', fontStyle: 'italic'}}>{item.price}</span>
+                  <button className='addButton'>ADD TO CART</button>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </section>
+      <button className='viewCartButton'>See the cart</button>
+      <div className='footer'>
+        <span style={{fontWeight: 'bold', fontSize: '30px', marginLeft: '60px'}}>The Generics</span>
+      </div>
     </div>
   );
 }
